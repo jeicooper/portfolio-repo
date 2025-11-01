d065da312f3c8770421bcd5e00345a4575a5a8b2
Joyce Orog <jeicooper@users.noreply.github.com>
2025-11-02 03:41:11 +0800

    Navbar Clean Up

--- index.php
+++ index.php
@@ -5,11 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jei's portfolio</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="header_style.css">
 </head>
 
 <body>
-    <?php include 'header.php' ?>
+    <header>
+        <nav class="nav">
+            
+        <div class="logo">Jeicooper</div>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact Me</a></li>
+            </ul>
+        </nav>
+
+    </header>   
 
     
 </body>
